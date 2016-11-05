90033c74ffeb87e4ea2fb492df4174c7dedf4e82
Polina Pasevina <rigiemp@gmail.com>
2016-11-05 15:52:32 +0300

    POST added

--- src/test/java/restapi/HelloRestAPI.java
+++ src/test/java/restapi/HelloRestAPI.java
@@ -1,6 +1,8 @@
 package restapi;
 
 import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
 import org.junit.Test;
 
 import static io.restassured.RestAssured.expect;
@@ -32,7 +34,34 @@ public class HelloRestAPI {
     public void postTest() {
         RestAssured.baseURI = "http://jsonplaceholder.typicode.com/posts";
 
-        
+        String myJson = "{\"id\":\"1\"}";
+
+        Response r = given()
+                .contentType("application/json").
+                        body("{\"id\":\"1\"}").
+                        when().
+                        post("");
+
+        String body = r.getBody().asString();
+        System.out.println(body);
+    }
+
+    @Test
+    public void updateTest() {
+        RestAssured.baseURI = "http://jsonplaceholder.typicode.com/posts/1";
+
+    }
+
+    @Test
+    public void deleteTest() {
+        RestAssured.baseURI = "http://jsonplaceholder.typicode.com/posts";
+    }
+
+    @Test
+    public void failedTest() {
+        given().
+                when().get("http://google.com/12").
+                then().statusCode(404);
     }
 
 }
