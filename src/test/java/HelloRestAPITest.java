dd6d070d870c9abe69c25389f4c2c0ad37f2608f
Polina Pasevina <rigiemp@gmail.com>
2016-11-26 22:01:18 +0300

    Changed pom plugin

--- src/test/java/HelloRestAPITest.java
+++ src/test/java/HelloRestAPITest.java
@@ -12,63 +12,7 @@ import static org.hamcrest.Matchers.equalTo;
  * Created by user on 05.11.2016.
  */
 public class HelloRestAPITest {
-//
-//
-//    private final String URL = "http://jsonplaceholder.typicode.com";
-//    private final String CONTENT_TYPE = "application/json";
-//
-//    @Test
-//    public void getPost1() {
-//        given().contentType(CONTENT_TYPE)
-//                .when()
-//                .get(URL + "/posts/1")
-//                .then()
-//                .body(containsString("userId"))
-//                .body(containsString("id"))
-//                .body(containsString("title"))
-//                .body(containsString("body"))
-//                .statusCode(200);
-//    }
-//
-//    @Test
-//    public void postPosts() {
-//        given().contentType(CONTENT_TYPE)
-//                .when()
-//                .post(URL + "/posts")
-//                .then()
-//                .body(containsString("id"))
-//                .statusCode(201);
-//    }
-//
-//    @Test
-//    public void putPosts1() {
-//        given().contentType(CONTENT_TYPE)
-//                .when()
-//                .put(URL + "/posts/1")
-//                .then()
-//                .body(containsString("id"))
-//                .statusCode(200);
-//    }
-//
-//    @Test
-//    public void deletePosts1() {
-//        given().contentType("application/json")
-//                .when()
-//                .delete(URL + "/posts/1")
-//                .then()
-//                .body(containsString(""))
-//                .statusCode(200);
-//    }
-//
-//    @Test
-//    public void incorrect() {
-//        given().contentType("application/json")
-//                .when()
-//                .delete(URL + "/posts/fail")
-//                .then()
-//                .body(containsString(""))
-//                .statusCode(404);
-//    }
+
     @BeforeClass
     public static void setBaseURI() {
         RestAssured.baseURI = "http://jsonplaceholder.typicode.com";
